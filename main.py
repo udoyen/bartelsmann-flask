26157f92dfa193435e08d4947b03ee64c05090f1
udoyen <datameshprojects@gmail.com>
2020-12-02 16:18:34 +0100

    finished deployement to google cloud run

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ from flask_cors import CORS
 
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 
-import json, xmljson
+import json
+import xmljson
 
 import requests
 
- 
+
 # initialise db
 
 
-
 app = Flask(__name__)
-CORS(app) ## To allow direct AJAX calls
+CORS(app)  # To allow direct AJAX calls
 
 # db configuration
 # app.config['MYSQL_HOST'] = 'localhost'
@@ -26,37 +26,36 @@ CORS(app) ## To allow direct AJAX calls
 # mysql = MySQL(app)
 
 
-
 # home page route and view, containing a dictionary thats rendered in the updates section
 @app.route('/')
 def Index():
 
     category = [
-             {
-                'id':1,
-                'title': 'Covid-19 Updates',
-                'description': "A centralised hub of all the latest covid-19 updates and statics from around the world and a selected few african states"
-             
-            
-            },
+        {
+            'id': 1,
+            'title': 'Covid-19 Updates',
+            'description': "A centralised hub of all the latest covid-19 updates and statics from around the world and a selected few african states"
 
-              {
-                'id':2,
-                'title': 'World Health News',
-                'description': "All the latest world health news aggregated in one central hub for you to freely access and stay informed on whats happening in the health world"
-            },
 
-              {
-                'id':3,
-                'title': 'Nigeria Health News',
-                'description': "Catch up to speed with the latest nigerian health news, collected from trusted publishers in nigeria, bringimg you both local and international ."
-            },
+        },
 
-            {
-                'id':4,
-                'title': 'South Africa Health News',
-                'description': "Catch up to speed with the latest South Africa health news, collected from trusted publishers in nigeria, bringimg you both local and international"
-            }
+        {
+            'id': 2,
+            'title': 'World Health News',
+            'description': "All the latest world health news aggregated in one central hub for you to freely access and stay informed on whats happening in the health world"
+        },
+
+        {
+            'id': 3,
+            'title': 'Nigeria Health News',
+            'description': "Catch up to speed with the latest nigerian health news, collected from trusted publishers in nigeria, bringimg you both local and international ."
+        },
+
+        {
+            'id': 4,
+            'title': 'South Africa Health News',
+            'description': "Catch up to speed with the latest South Africa health news, collected from trusted publishers in nigeria, bringimg you both local and international"
+        }
 
 
     ]
@@ -64,35 +63,37 @@ def Index():
     return render_template('home.html', news_categories=category)
 
 
-# api call to news api for world nes, its only a get request 
+# api call to news api for world nes, its only a get request
 @app.route('/worldhealthnews', methods=['GET'])
 def world_health_news():
-    r = requests.get('http://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=db0dbc6d89ae4a8583fb8c71a49deb8b').json()
-    
+    r = requests.get(
+        'http://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=db0dbc6d89ae4a8583fb8c71a49deb8b').json()
+
     data = r.get('articles')
 
-    return render_template('news.html', data = data )  
-
+    return render_template('news.html', data=data)
 
 
 # api call for nigeria news
 @app.route('/nigeria_news')
 def nigeria_news():
-    data = requests.get('http://newsapi.org/v2/top-headlines?country=ng&category=health&apiKey=db0dbc6d89ae4a8583fb8c71a49deb8b').json()
+    data = requests.get(
+        'http://newsapi.org/v2/top-headlines?country=ng&category=health&apiKey=db0dbc6d89ae4a8583fb8c71a49deb8b').json()
 
     data = data.get('articles')
 
-    return render_template('nigeria_news.html', data = data )  
+    return render_template('nigeria_news.html', data=data)
 
 
-# api call for outh africa health news all still via news api   
+# api call for outh africa health news all still via news api
 @app.route('/sa_news')
 def sa_news():
-    data = requests.get('http://newsapi.org/v2/top-headlines?country=za&category=health&apiKey=db0dbc6d89ae4a8583fb8c71a49deb8b').json()
-    
+    data = requests.get(
+        'http://newsapi.org/v2/top-headlines?country=za&category=health&apiKey=db0dbc6d89ae4a8583fb8c71a49deb8b').json()
+
     data = data.get('articles')
 
-    return render_template('sa_news.html', data = data )  
+    return render_template('sa_news.html', data=data)
 
 
 # api call to covid19api.com , it feteches alll sumaeries of the pandemic , the same funtion calls covid19 updates from uganda , south africa nd nigeria
@@ -100,26 +101,29 @@ def sa_news():
 @app.route('/covidstats', methods=['GET'])
 def covid_stats():
     r = requests.get('https://api.covid19api.com/summary').json()
-    south_live = requests.get('https://api.covid19api.com/total/country/south-africa').json()
-    uganda_live = requests.get('https://api.covid19api.com/total/country/uganda').json()
-    nigeria_live = requests.get('https://api.covid19api.com/total/country/nigeria').json()
-                                
+    south_live = requests.get(
+        'https://api.covid19api.com/total/country/south-africa').json()
+    uganda_live = requests.get(
+        'https://api.covid19api.com/total/country/uganda').json()
+    nigeria_live = requests.get(
+        'https://api.covid19api.com/total/country/nigeria').json()
+
     data = r
     south_data = south_live
     uganda_data = uganda_live
     nigeria_data = nigeria_live
 
-    return render_template('covidstats.html', data = data, south_data = south_data, uganda_data = uganda_live, nigeria_data =nigeria_live )  
+    return render_template('covidstats.html', data=data, south_data=south_data, uganda_data=uganda_live, nigeria_data=nigeria_live)
 
 
 # creating user admin for the first time,
 class RegisterForm(Form):
     name = StringField('Name', [validators.Length(min=1, max=50)])
     email = StringField('email', [validators.Length(min=1, max=50)])
-    password = PasswordField('Password', [validators.DataRequired(), validators.EqualTo('confirm', message='Passwords doesnt match')])
+    password = PasswordField('Password', [validators.DataRequired(
+    ), validators.EqualTo('confirm', message='Passwords doesnt match')])
     confirm = PasswordField('confirm Password')
 
-    
 
 # @app.route('/register', methods=['GET', 'POST'])
 # def register():
@@ -148,14 +152,13 @@ class RegisterForm(Form):
 #     return render_template('register.html', form=form)
 
 
-#using the mailgun api, a user subcribes for email listing
+# using the mailgun api, a user subcribes for email listing
 def subscribe_user(email, user_group_email, api_key):
     r = requests.post(f"https://api.mailgun.net/v3/lists/{user_group_email}/members",
-                auth=("api", api_key),
-                  data={"subscribed": True,
-                        "address": email}
-    )
-
+                      auth=("api", api_key),
+                      data={"subscribed": True,
+                            "address": email}
+                      )
 
     print(r.status_code)
     return r
@@ -164,16 +167,16 @@ def subscribe_user(email, user_group_email, api_key):
 @app.route('/subscribe', methods=["GET", "POST"])
 def subcribe():
 
-    if request.method =="POST":
-        email =  request.form.get('email')
-        subscribe_user(email = email, 
-                        user_group_email="healthrocy@sandbox30d1b9d9f64c4c35832b400b6bd46e66.mailgun.org",
-                        api_key="0d12038e0871eb4f63aabc33f5d970f1-9b1bf5d3-0baad249"  )
-        
+    if request.method == "POST":
+        email = request.form.get('email')
+        subscribe_user(email=email,
+                       user_group_email="healthrocy@sandbox30d1b9d9f64c4c35832b400b6bd46e66.mailgun.org",
+                       api_key="0d12038e0871eb4f63aabc33f5d970f1-9b1bf5d3-0baad249")
 
     return render_template('subcribe.html')
 
+
 if __name__ == '__main__':
 
-    app.secret_key="secret123"
-    app.run(debug=True)
+    app.secret_key = "secret123"
+    app.run(debug=True, host='0.0.0.0', port=8080)
