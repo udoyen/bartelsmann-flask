1d468c9aa2b244b479082e54a1de71076ef3e56e
udoyen <datameshprojects@gmail.com>
2020-12-11 14:43:47 +0100

    updated the port to 80

--- main.py
+++ main.py
@@ -179,4 +179,4 @@ def subcribe():
 if __name__ == '__main__':
 
     app.secret_key = "secret123"
-    app.run(debug=True, host='0.0.0.0', port=3000)
+    app.run(debug=True, host='0.0.0.0', port=80)
